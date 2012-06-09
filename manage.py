ccae07cb01ac59d55f47816b0303e19a116ac22b
Dave Pedowitz <github@alishabeth.com>
2012-06-09 06:57:09 -0700

    update the app runner

--- manage.py
+++ manage.py
@@ -1,11 +1,2 @@
-#!/usr/bin/env python
-from __future__ import absolute_import
-
-from flaskext.script import Manager
-
 from supportsomething import app
-
-app.run()
-#manager = Manager(app)
-
-#manager.run()
+app.run(host=app.config.get('HOST', '127.0.0.1'), port=app.config.get('PORT', 5000))
