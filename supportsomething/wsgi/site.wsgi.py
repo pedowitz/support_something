ced8cc0c33bbc406f9d89361c92025609fb4cbba
Dave Pedowitz <github@alishabeth.com>
2012-06-10 07:49:08 -0700

    added wsgi

---
+++ supportsomething/wsgi/site.wsgi.py
@@ -0,0 +1,11 @@
+import sys
+
+activate_this = '/home/ubuntu/workspace/support_something/env/bin/activate_this.py'
+execfile(activate_this, dict(__file__=activate_this))
+
+source_this = '/home/ubuntu/workspace/support_something/settings.py'
+execfile(source_this, dict(__file__=source_this))
+
+sys.path.insert(0, '/home/ubuntu/workspace/support_something')
+
+from supportsomething import app as application
