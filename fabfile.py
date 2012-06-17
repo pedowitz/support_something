bb5fc97479f8d65ca3fa4001fc8cbda740e11a34
Dave Pedowitz <github@alishabeth.com>
2012-06-16 17:38:18 -0700

    deployment support

--- fabfile.py
+++ fabfile.py
@@ -16,3 +16,18 @@ def _virtualenv(command, *args):
     result = local('. env/bin/activate&& ' + command + ' ' + ' '.join(args),
                    capture=False)
     return result
+
+path = '/home/ubuntu/workspace/support_something'
+def update_prod():
+    _prod()
+    with cd(path):
+        run('git pull --rebase')
+        run('touch supportsomething/wsgi/site.wsgi')
+
+def run_app():
+    _prod()
+    with cd(path):
+        run('export SETTINGS=/home/ubuntu/workspace/settings.py && . env/bin/activate&& python manage.py > ~/log &')
+
+def _prod():
+    env.host_string = 'ubuntu@ec2-23-22-24-40.compute-1.amazonaws.com'
